6befe2698b52950160d6478e4019509902b7d927
Albertoperleo <albperleo@alum.us.es>
2022-12-27 18:20:00 +0100

    Creada la función de identificación de la nota

--- recognition/pitch.py
+++ recognition/pitch.py
@@ -1,3 +1,4 @@
+from recognition.note_freq import NOTE_FREQUENCIES
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -16,15 +17,18 @@ def show_audio(audio_data, data, samples):
             
         # Calculate pitch
         spectrum = calculate_pitch(data, samples)
+        note = calculate_note(spectrum)
+        print(max(spectrum))
+        print(note)
         
-        # Show audio
-        plt.plot(spectrum)
-        plt.ylim(0,10)
+        # Show audio with log expression
+        plt.plot(10*np.log10(spectrum))
         plt.show(block=False)
-        plt.pause(0.01)
+        plt.pause(0.5)
         plt.clf()
-        plt.xlabel('Frequency')
-        plt.ylabel('Amplitude')
+        plt.title(note)
+        plt.xlabel('Freq. (Hz)')
+        plt.ylabel('Pot. (dB)')
         
 
 def calculate_pitch(data, samples):
@@ -42,10 +46,31 @@ def calculate_pitch(data, samples):
     # DFT data
     data_dft = np.fft.fft(data)
     
-    # Amplitude
+    # Spectrum
     spectrum = np.abs(data_dft)
     spectrum = spectrum/samples
     
-    return spectrum
+    # Logaritmic expression
+    spectrum = spectrum**2
     
+    return spectrum
+
+
+def calculate_note(spectrum):
+    
+    note = 'None'
+    note_max = max(spectrum)
+    freq_values = NOTE_FREQUENCIES.values()
+    
+    if note_max >= min(freq_values):
+        for k,v in NOTE_FREQUENCIES.items():
+            cont = 0
+            while(note_max/2 >= min(freq_values)):
+                cont += 1
+                note_max = note_max / 2
+            if note_max >= v-0.5 and note_max <= v-0.5:
+                note = k[0] + str(cont)
+                
+    return note
+        
     
